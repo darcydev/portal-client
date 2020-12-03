1e6f4f50fa2eb80a301964d38285197029807e24
Darcy Price <darcyelsing@gmail.com>
2020-12-03 16:00:25 +1100

    removed create new brief button

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
 import Link from 'next/link';
-import { Button } from 'antd';
+// import { Button } from 'antd';
 
-import { createBrief } from '../lib/briefs';
+// import { createBrief } from '../lib/briefs';
 
 export default function Index() {
 	return (
 		<div>
 			<h1>home page</h1>
-			<Button onClick={() => createBrief()}>create new brief</Button>
+			{/* <Button onClick={() => createBrief()}>create new brief</Button> */}
 			<Link href='/clients'>view all clients</Link>
 			<Link href='/media/upload'>upload media</Link>
 		</div>
